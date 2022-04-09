cce7c15c1be4e482d875eb3594bf47c812489444
Santiago-Galvis <Santiagogalvisgt@gmail.com>
2022-04-08 21:03:45 -0500

    Proyect superhero.

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-/* Importacion de librerias */
 const express = require('express');
 const { default: mongoose } = require('mongoose');
-require('dotenv').config();
-const {
-  logErrors,
-  errorHandler,
-  boomErrorHandler,
-} = require('./src/middlewares/handlers/errors.handlers');
-
-/* importacion de rutas del proyceto*/
-const routerApi = require('./src/routes');
-const port = process.env.PORT;
 const app = express();
+const port = process.env.port || 3000;
+const routerApi = require('./src/routes')
 
-/* activacion del puerto */
+require("dotenv").config();
+
 app.listen(port, () => console.log('Listen port', port));
-
-/* conectamos con la base de datos */
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING)
-  .then(() => console.log('Success connection with mongo'))
-  .catch(() => console.error('Connection could not be established'));
-
-/* creacion de middleware, nos eprmite usar el next en el archivo error.handler*/
-app.use(logErrors);
-app.use(errorHandler);
-app.use(boomErrorHandler);
 app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(() => console.log("Succes connection with mongo"))
+    .catch(() => console.error("Connection could not be established"));
+
 routerApi(app);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const serie_routes = require('./series.routes');
+const peopleRoutes = require("./person.router")
+const departamentsRoutes = require('./departaments.router')
+const superheroRoutes = require('./superhero.router')
 
 function routerApi(app){
-    const dynamic_routes = express.Router();
-    app.use("/api/v2", dynamic_routes); /* endpoint http://localhost:3000/api/v2 */
-    dynamic_routes.use('/series', serie_routes);
+    const routes = express.Router();
+    app.use("/api/v1", routes); /* endpoint http://localhost:3000/api/v1 */
+    routes.use("/people", peopleRoutes); /* Endpoint http://localhost:3000/api/v1/people */
+    routes.use('/departaments', departamentsRoutes); /* Endpoint http://localhost:3000/api/v1/departaments */
+    routes.use('/superheros', superheroRoutes); /* Endpoint http://localhost:3000/api/v1/superheros */
 }
 
 module.exports = routerApi;
